efce1094840e25c7000f76a9fce0a51377bfaf79
Colin Frerichs <colin.frerichs@gmail.com>
2017-03-16 13:06:44 +0000

    last second demo changes:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,11 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-<<<<<<< HEAD
 //= require_tree .
-=======
-//= require_tree .
-@import jquery
-@import bootstrap-sprockets
-
->>>>>>> right-choice
