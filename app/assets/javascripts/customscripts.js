68d85da23c766cf8071861dbf582743d1b868d70
Colin Frerichs <colin.frerichs@gmail.com>
2017-03-21 21:24:25 +0000

    updated elementary page

--- app/assets/javascripts/customscripts.js
+++ app/assets/javascripts/customscripts.js
@@ -1,3 +1,4 @@
+/* global $ */
 
 var presentationSummary = document.getElementById("presentation-summary");
 
@@ -16,5 +17,3 @@ $("#elementarylink").fadeIn("slow",function(){});
 $( "p" ).click(function() {
   $( "p" ).fadeOut( "slow" );
 });
-
-/* global $ */
